e21420b0308900399dda907d78386e46aa054b25
Gen <136227563+GenericUser8@users.noreply.github.com>
2024-12-20 15:28:34 +0800

    Added cart item count to navbar.

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -256,6 +256,7 @@ export default function ProductList() {
     fontSize: '30px',
     textDecoration: 'none',
   }
+
   function handleCartClick(e: React.MouseEvent) {
     e.preventDefault();
     setShowCart(true); // Set showCart to true when cart icon is clicked
@@ -278,6 +279,15 @@ export default function ProductList() {
     }));
   }
 
+  /** Get the current amount of items in the cart. */
+  function getCartItemsCount(): number {
+    let count = 0;
+    for (const item of cart) {
+      count += item.quantity;
+    }
+    return count
+  }
+
   return (
     <div>
       <div className="navbar" style={styleObj}>
@@ -295,7 +305,7 @@ export default function ProductList() {
         </div>
         <div style={styleObjUl}>
           <div> <a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-          <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
+          <div> <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}><h1 className='cart'> {getCartItemsCount()} <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68"><rect width="156" height="156" fill="none"></rect><circle cx="80" cy="216" r="12"></circle><circle cx="184" cy="216" r="12"></circle><path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" id="mainIconPathAttribute"></path></svg></h1></a></div>
         </div>
       </div>
       {!showCart ? (
